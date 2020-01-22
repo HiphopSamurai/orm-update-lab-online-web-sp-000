f65145a6b379a950fb09f209691e34685f6652ae
Garrett Thompson <gtcruz.gt@gmail.com>
2020-01-22 03:52:49 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -5,5 +5,40 @@ class Student
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
 
+  def initialize(id=nil, name, grade)
+    #
+  end
 
+  def self.create_table
+    #
+  end
+
+  def self.drop_table
+    #
+  end
+
+  def save
+    #
+  end
+
+  def self.create
+    #
+  end
+
+  def self.new_from_db(array)
+    # This class method takes an argument of an array.
+    # When we call this method we will pass it the array that
+    # is the row returned from the database by the execution of a
+    # SQL query. We can anticipate that this array will contain
+    #cthree elements in this order: the id, name and grade of a
+    # student.
+  end
+
+  def self.find_by_name(name)
+    #
+  end
+
+  def update
+    #
+  end
 end
